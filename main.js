6a8072b6f3be465d2a86aebec3d912ac19808181
pacebook <haegyun821@gmail.com>
2017-10-24 00:48:17 +0900

    init

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+
+app.use(express.static('public'));
+
+const GRAPH_URL = 'https://graph.facebook.com'
+const APP_ID = process.env.FB_APP_ID;
+const SECRET = process.env.FB_SECRET;
+const CAFE_OTTIMO = 'ottimofood';
+
+let access_token;
+
+app.listen(3000, () => {
+    request(`${GRAPH_URL}/oauth/access_token?client_id=${APP_ID}&client_secret=${SECRET}&grant_type=client_credentials`, 
+            (error, response, body) => {
+                access_token = JSON.parse(body).access_token;
+                console.log(access_token);
+            });
+    console.log('Node server started.');
+})
+
+app.get('/api/menus', (req, res) => {
+    request(`${GRAPH_URL}/v2.9/${CAFE_OTTIMO}/feed?fields=attachments&access_token=${access_token}`, (error, response, body) => {
+        res.send(body);
+    })
+    // res.send('only route');
+})
