2ef5c1b62382eb842fbf3f9747d34bf1b1a41e48
pacebook <haegyun821@gmail.com>
2017-12-17 16:14:43 +0900

    check port

--- main.js
+++ main.js
@@ -9,17 +9,18 @@ const GRAPH_URL = 'https://graph.facebook.com'
 const APP_ID = process.env.FB_APP_ID;
 const SECRET = process.env.FB_SECRET;
 const CAFE_OTTIMO = 'ottimofood';
+const PORT = process.env.PORT || 3000;
 
 let access_token;
 
 app.use(cors());
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(PORT, () => {
     request(`${GRAPH_URL}/oauth/access_token?client_id=${APP_ID}&client_secret=${SECRET}&grant_type=client_credentials`, 
             (error, response, body) => {
                 access_token = JSON.parse(body).access_token;
             });
-    console.log('Node server started.');
+    console.log(`Node server started at ${PORT}`);
 })
 
 app.get('/api/menus', (req, res) => {
